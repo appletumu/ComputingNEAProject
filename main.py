238075be58ba25e0502e578e90f939ef54aa8421
AppleTumu <singhh-19@gbr.merciantrust.org.uk>
2025-08-26 14:28:37 +0100

    yes

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 import tkinter as tk
-from root import *
+from window import *
 
-root = Window("Quiz Game")
+window = Window("Quiz Game")
 
-question_label = tk.Label(root, text="What is the capital of France?", font=("Arial", 14))
+question_label = tk.Label(window, text="What is the capital of France?", font=("Arial", 14))
 question_label.pack(pady=10)
 
 # Answer options
@@ -11,7 +11,7 @@ answer_var = tk.StringVar()
 
 options = ["Paris", "London", "Berlin", "Rome"]
 for option in options:
-    tk.Radiobutton(root, text=option, variable=answer_var, value=option).pack(anchor="w")
+    tk.Radiobutton(window, text=option, variable=answer_var, value=option).pack(anchor="w")
 
 # Submit button
 def check_answer():
@@ -20,11 +20,11 @@ def check_answer():
     else:
         result_label.config(text="Wrong!", fg="red")
 
-submit_btn = tk.Button(root, text="Submit", command=check_answer)
+submit_btn = tk.Button(window, text="Submit", command=check_answer)
 submit_btn.pack(pady=10)
 
 # Result
-result_label = tk.Label(root, text="")
+result_label = tk.Label(window, text="")
 result_label.pack()
 
-root.mainloop()
+window.mainloop()
