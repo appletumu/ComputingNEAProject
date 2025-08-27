95e71e4841b5bc6c4cf69cc622ad68f324175612
AppleTumu <singhh-19@gbr.merciantrust.org.uk>
2025-08-27 08:12:08 +0100

    Update window.py

--- window.py
+++ window.py
@@ -5,3 +5,9 @@ class Window(tk.Tk):
         super().__init__()
 
         self.title(title)
+
+class Components(Window):
+    def __init__(self, ):
+        super().__init__()
+
+        # Wil contain buttons, and stuff
