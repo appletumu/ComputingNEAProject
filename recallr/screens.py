10964d28189d611b29dd4a7f79e460aefbd72f75
AppleTumu <singhh-19@gbr.merciantrust.org.uk>
2025-10-10 23:07:37 +0100

    .

--- recallr/screens.py
+++ recallr/screens.py
@@ -1,14 +1,22 @@
 from recallr.ui_elements import Frame, Component
 
+def setup_screen(func, screen_type="menu"):
+    def wrapper(self, *args, **kwargs):
+        frame = Frame(self.screen_manager)
+        component = Component(frame)
+
+        # Call the decorated screen method
+        func(self, component, *args, **kwargs)
+
+        self.screen_manager.frames.append(frame)
+    return wrapper
 
 class Screens:
     def __init__(self, screen_manager):
         self.screen_manager = screen_manager
 
-    def login(self):
-        frame = Frame(self.screen_manager)
-        component = Component(frame)
-
+    @setup_screen(screen_type="menu")
+    def login(self, component):
         component.title(text="Recallr")
         component.content(text="Please fill in your login details!")
         component.entry_field(placeholder_text="Username")
@@ -16,12 +24,8 @@ class Screens:
         component.button(text="Login")
         component.button(text="Create account", button_type="grey")
 
-        self.screen_manager.frames.append(frame)
-
-    def create_account(self):
-        frame = Frame(self.screen_manager)
-        component = Component(frame)
-
+    @setup_screen(screen_type="menu")
+    def create_account(self, component):
         component.title(text="Create Account")
         component.content(text="Please fill in the following details!")
         component.entry_field(placeholder_text="New Username")
@@ -30,17 +34,11 @@ class Screens:
         component.button(text="Create account")
         component.button(text="Cancel", button_type="red")
 
-        self.screen_manager.frames.append(frame)
-
-    def main_menu(self):
-        frame = Frame(self.screen_manager)
-        component = Component(frame)
-
+    @setup_screen(screen_type="menu")
+    def main_menu(self, component):
         component.title(text="Recallr")
         component.content(text="Hello, {username}!")
         component.button(text="Notes")
         component.button(text="Test yourself")
         component.button(text="Settings", button_type="grey")
         component.button(text="Sign out", button_type="red")
-
-        self.screen_manager.frames.append(frame)
