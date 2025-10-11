100a35a53828e4a84c65a11886a0ff915e23e49e
AppleTumu <singhh-19@gbr.merciantrust.org.uk>
2025-10-11 19:48:40 +0100

    .

--- algorithms/randomisation_algorithm.py
+++ algorithms/randomisation_algorithm.py
@@ -1,20 +1,12 @@
 import random
 
+def derangement(items):
+    while True:
+        shuffled = items[:]
+        random.shuffle(shuffled)
+        if all(a != b for a, b in zip(items, shuffled)):
+            return shuffled
+
 items = input("Enter items separated by commas: ").split(', ')
-randomised_items = []
-change_pos = []
-
-i = 0
-for i in range(len(items)):
-    possible_pos = random.randint(0, len(items)-1)
-    
-    if possible_pos in change_pos:
-        continue
-
-    change_pos.append(possible_pos)
-    i += 1
-
-for item in items:
-    randomised_items.append(items[change_pos[len(randomised_items)]])
-
-print(randomised_items)
+result = derangement(items)
+print(result)
